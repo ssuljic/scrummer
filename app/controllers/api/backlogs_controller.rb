29295168cb52fe1b28335b539a47bafc6f273b04
Mirha Zec <mzec1@etf.unsa.ba>
2015-03-23 16:22:32 +0100

    Add comment

--- app/controllers/api/backlogs_controller.rb
+++ app/controllers/api/backlogs_controller.rb
@@ -2,8 +2,9 @@ class Api::BacklogsController < ApiController
   before_filter :restrict_api_access
 
   def index
+    #found_tickets =Ticket.where(id: params[:project_id]).independent
     found_user_stories=UserStory.where(project_id: params[:project_id])
     found_sprints=Sprint.where(project_id: params[:project_id])
-    render response: { :user_stories=>found_user_stories,:sprints =>found_sprints}
+    render response: {:user_stories=>found_user_stories,:sprints =>found_sprints}
   end
 end
