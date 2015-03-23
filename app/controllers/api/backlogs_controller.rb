908dc70fbbc53a82c433a5be95c469efbbdfb1a3
Mirha Zec <mzec1@etf.unsa.ba>
2015-03-23 16:15:53 +0100

    Small changes

--- app/controllers/api/backlogs_controller.rb
+++ app/controllers/api/backlogs_controller.rb
@@ -2,9 +2,8 @@ class Api::BacklogsController < ApiController
   before_filter :restrict_api_access
 
   def index
-
     found_user_stories=UserStory.where(project_id: params[:project_id])
     found_sprints=Sprint.where(project_id: params[:project_id])
-    render response: {  :user_stories=>found_user_stories,:sprints =>found_sprints}
+    render response: { :user_stories=>found_user_stories,:sprints =>found_sprints}
   end
 end
