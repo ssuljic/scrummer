1064572b4df3e4559da07580adfe15a48400b509
zulicn <zulicn@gmail.com>
2015-03-29 21:10:27 +0200

    Dashboard Angular service added.

--- app/assets/javascripts/services/services.js
+++ app/assets/javascripts/services/services.js
@@ -5,13 +5,13 @@ var services = angular.module('services', ['ngResource']);
 // Factory for users
 services.factory('usersFactory', function ($resource) {
   return $resource('api/users', {}, {
-      create: { method: 'POST' }
+    create: { method: 'POST' }
   })
 });
 
 // Factory for dashboard
 services.factory('dashboardFactory', function ($resource) {
   return $resource('api/dashboard', {}, {
-      get: { method: 'GET' }
+    get: { method: 'GET' }
   })
 });
