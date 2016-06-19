fd8e84c16293776f5bf90e70d1ce7d29f6dd938b
ssuljic <sadzid.etf@gmail.com>
2016-06-19 12:35:47 +0200

    Added confirmation dialog on logout

--- app/assets/javascripts/directives/viewDirectives.js
+++ app/assets/javascripts/directives/viewDirectives.js
@@ -1,6 +1,5 @@
 var view_directives = angular.module('view_directives', []);
 
-
 // Page header view directive
 view_directives.directive('header', ['$translate', function() {
   return {
@@ -10,16 +9,23 @@ view_directives.directive('header', ['$translate', function() {
       description: '=',
       logged_user: '='
     },
-    link: function($scope, $element) {},
+    link: function($scope, $element) {
+      $scope.logout = function() {
+        var result = confirm('Are you sure you want to log out?');
+        if(result) {
+          window.location = "/#/logout"
+        }
+      }
+    },
     template:'<div class="page-header" id="header">' +
               '<p class="header">' +
                 '{{title | translate}}' +
                 '<small class="desc">' +
                   ' {{description.substring(0, 50)}}' +
                 '</small>' +
-                '<a ng-href="/#/logout"><span class="glyphicon glyphicon-log-out icon"></span></a>' +
-                '<a ng-href="/#/inbox"><span class="glyphicon glyphicon-envelope icon"></span></a>' +
-                '<a ng-href="/#/dashboard"><span class="glyphicon glyphicon-home icon"></span></a>' +
+                '<a ng-click="logout()"><span class="header-nav glyphicon glyphicon-log-out icon"></span></a>' +
+                '<a ng-href="/#/inbox"><span class="header-nav glyphicon glyphicon-envelope icon"></span></a>' +
+                '<a ng-href="/#/dashboard"><span class="header-nav glyphicon glyphicon-home icon"></span></a>' +
               '</p>' +
             '</div>',
     controller: function($scope, $element, $translate) {
